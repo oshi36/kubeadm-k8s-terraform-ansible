cabbfdb98ab3a1b4670f918506e5b5e6cb7800e4
Oshi Gupta <oshiagupta36@gmail.com>
2024-01-05 07:04:41 +0000

    added terraform scripts to spin single node kubeadm k8s cluster

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,3 @@ output "master" {
   value = aws_instance.k8s_master.public_ip
 }
 
-output "workers" {
-  value = aws_instance.k8s_worker[*].public_ip
-}
--- variables.tf
+++ variables.tf
@@ -6,7 +6,6 @@ variable "ami" {
   type = map(string)
   default = {
     master = "ami-00d8834ea9c9ecd09"
-    worker = "ami-00d8834ea9c9ecd09"
   }
 }
 
@@ -14,11 +13,6 @@ variable "instance_type" {
   type = map(string)
   default = {
     master = "t2.medium"
-    worker = "t2.micro"
   }
 }
 
-variable "worker_instance_count" {
-  type    = number
-  default = 1
-}
